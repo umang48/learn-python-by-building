d6cc2f23f0ba02c60ff342c2a69a1ca354d07d44
umang <umangitdeveloper@gmail.com>
2026-07-04 16:22:36 +0530

    data types program and operators program

---
+++ basics/data_types.py
@@ -0,0 +1,9 @@
+name = "Umang"
+age = 25
+height = 5.8
+is_developer = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_developer))
---
+++ basics/operators.py
@@ -0,0 +1,10 @@
+a = 20
+b = 10
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
