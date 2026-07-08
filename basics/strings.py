05c234fbe9379dfdff7138cf5b8555830077ce1a
umang <umangitdeveloper@gmail.com>
2026-07-09 00:05:04 +0530

    string program0

---
+++ basics/strings.py
@@ -0,0 +1,9 @@
+name = "Umang Prajapati"
+
+print("Original:", name)
+print("Length:", len(name))
+print("Upper:", name.upper())
+print("Lower:", name.lower())
+print("First Character:", name[0])
+print("Last Character:", name[-1])
+print("Contains 'Prajapati':", "Prajapati" in name)
